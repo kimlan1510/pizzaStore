ddf5d1718a60626ae32b3dae599b1a423a9a419f
Kimlan Nguyen <kimlan1510@yahoo.com>
2017-05-12 11:38:01 -0700

    Add formula to calculate price, add selectors to UI, display final price on html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+  $("form#size").submit(function(event){
+    event.preventDefault();
+    newPizza.Size = $("input:radio[name=size]:checked").val();
+    console.log(newPizza.Size);
+  });
+  $("form#cheese").submit(function(event){
+    event.preventDefault();
+    newPizza.Cheese = $("input:radio[name=cheese]:checked").val();
+    console.log(newPizza.Cheese);
+  });
+  $("form#toppings").submit(function(event){
+    event.preventDefault();
+    newPizza.Toppings = [];
+    $(":checkbox:checked").each(function(i){
+          newPizza.Toppings[i] = $(this).val();
+    });
+    console.log(newPizza.Toppings);
+    var price = newPizza.pizzaPrice(newPizza.Size, newPizza.Cheese, newPizza.Toppings);
+    $("#result").text("$" + price);
+    console.log(price);
+  });
 
 });
 
@@ -7,15 +28,25 @@ $(document).ready(function(){
 function pizza(size, cheese, toppings){
   this.Size = size;
   this.Cheese = cheese;
-  this.Toppings = [];
+  this.Toppings = toppings;
 }
+// pizza object
+var newPizza = new pizza("size","cheese","toppings");
 
+//method to calculate pizza price
 pizza.prototype.pizzaPrice = function(size, cheese, toppings){
-  var price;
+  var price = 0;
   if(cheese === 0){
-    price = 3*size + 3.5*toppings.length ;    
+    for(i=0; i<newPizza.Toppings.length; i++){
+      price += parseInt(newPizza.Toppings[i]);
+    }
+    price += parseInt(size)*price;
   }
   else{
-    price =  3*size + 3.5*toppings.length + 1;
+    for(i=0; i<newPizza.Toppings.length; i++){
+      price += parseInt(newPizza.Toppings[i]);
+    }
+    price += (parseInt(size) + 1)*price;
   }
+  return price;
 }
