d66761871973fbf7ffd5c4b54daa17023ab2147f
Kimlan Nguyen <kimlan1510@yahoo.com>
2017-05-12 14:08:45 -0700

    Add methods to show toppings on html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+  $("#back_button1").click(function(){
+    $("#size").show();
+    $("#cheese").hide();
+  });
+  $("#back_button2").click(function(){
+    $("#toppings").hide();
+    $("#cheese").show();
+  });
   $("form#size").submit(function(event){
     event.preventDefault();
     newPizza.Size = $("input:radio[name=size]:checked").val();
+    $("#choice_made").append
     $("#cheese").show();
     $("#size").hide();
     console.log(newPizza.Size);
@@ -9,6 +18,8 @@ $(document).ready(function(){
   $("form#cheese").submit(function(event){
     event.preventDefault();
     newPizza.Cheese = $("input:radio[name=cheese]:checked").val();
+    $("#toppings").show();
+    $("#cheese").hide();
     console.log(newPizza.Cheese);
   });
   $("form#toppings").submit(function(event){
@@ -51,4 +62,44 @@ pizza.prototype.pizzaPrice = function(size, cheese, toppings){
     price += (parseFloat(size) + 1)*price;
   }
   return price;
+}
+
+pizza.prototype.showsize = function(size){
+  if(size === "2"){
+    var sizeChoice = "10 inches diameter";
+  }else if(size === "2.25"){
+    var sizeChoice = "16 inches diameter";
+  }else if(size === "2.5"){
+    var sizeChoice = "24 inches diameter";
+  }else if(size === "2.75"){
+    var sizeChoice = "30 inches diameter";
+  }else if(size === "3"){
+    var sizeChoice = "48 inches diameter";
+  }
+  return sizeChoice;
+}
+pizza.prototype.showcheese = function(cheese){
+  if(cheese === "1"){
+    var cheeseChoice = "Yes"
+  }else{
+    var cheeseChoice = "No"
+  }
+  return cheeseChoice;
+}
+pizza.prototype.showToppings = function(toppings){
+  var toppingArray = [];
+  for(i=0; i<toppings.length; i++){
+    if(toppings[i] === "1"){
+      toppingArray.push("Classic Pepperoni");
+    }else if(toppings[i] === "2"){
+      toppingArray.push("Italian Sausage");
+    }else if(toppings[i] === "3"){
+      toppingArray.push("Bacon");
+    }else if(toppings[i] === "4"){
+      toppingArray.push("Slice and dice chicken");
+    }else{
+      toppingArray.push("Steak");
+    }
+  }
+  return toppingArray;
 }
